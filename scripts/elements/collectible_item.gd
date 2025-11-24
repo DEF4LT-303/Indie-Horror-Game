4e2d497a45e423b07da1d16947659d3e36d8bb66
DEF4LT-303 <ryanrafi72@gmail.com>
2025-11-24 23:56:49 +0600

    fix dynamic dialogue system

--- scripts/elements/collectible_item.gd
+++ scripts/elements/collectible_item.gd
@@ -5,6 +5,8 @@ extends Node2D
 @export var item_name: String = "" 
 @export var dialogue_resource: DialogueResource
 
+var monitoring_pickup := false
+
 func _ready():
 	if GlobalState.is_item_visible(item_name) == false:
 		$".".visible = false
@@ -18,10 +20,20 @@ func _ready():
 		return
 
 	actionable.action_requested.connect(_on_action)
+	
+func _process(_delta):
+	if monitoring_pickup:
+		if GlobalState.is_item_collected(item_name):
+			# This triggers immediately when player chooses “Pick up”
+			visible = false
+			collision.disabled = true
 
 func _on_action(dialogue_resource, dialogue_start):
-	DialogueManager.show_dialogue_balloon(dialogue_resource, dialogue_start)
+	DialogueManager.show_dialogue_balloon(dialogue_resource, dialogue_start, [{"item_name": item_name}])
 	
-	if item_name != "":
-		GlobalState.mark_item_collected(item_name)
+	monitoring_pickup = true
+	await DialogueManager.dialogue_ended
+	monitoring_pickup = false
+	
+	if GlobalState.is_item_collected(item_name):
 		queue_free()
